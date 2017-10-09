c2d3569b7144e9b789e52efc82ecf586563e8049
DianaQuispe <danaalienmas@gmail.com>
2017-10-09 14:08:16 -0500

    act de homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css'; 
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 import {
   BrowserRouter,
@@ -9,9 +9,9 @@ import {
   NavLink,
   NavNavLink,
   Redirect
-} from 'react-router-dom'
-"use stritc";
+} from "react-router-dom";
 
+("use stritc");
 
 const TeacherList = [
   {
@@ -216,183 +216,189 @@ const JSCourses = [
   }
 ];
 
-
-
-
 const Home = () => (
   <div className="main-content home">
-        <h2>Front End Course Directory</h2>
+    <h2>Front End Course Directory</h2>
 
-        <p>
-          This fun directory is a project for the <em>React Router Basics</em>{" "}
-          course on Treehouse.
-        </p>
-        <p>
-          Learn front end web development and much more! This simple directory
-          app offers a preview of our course library. Choose from many hours of
-          content, from HTML to CSS to JavaScript. Learn to code and get the
-          skills you need to launch a new career in front end web development.
-        </p>
-        <p>
-          We have thousands of videos created by expert teachers on web design
-          and front end development. Our library is continually refreshed with
-          the latest on web technology so you will never fall behind.
-        </p>
-        <hr />
-      </div>
-)
+    <p>
+      This fun directory is a project for the <em>React Router Basics</em>{" "}
+      course on Treehouse.
+    </p>
+    <p>
+      Learn front end web development and much more! This simple directory app
+      offers a preview of our course library. Choose from many hours of content,
+      from HTML to CSS to JavaScript. Learn to code and get the skills you need
+      to launch a new career in front end web development.
+    </p>
+    <p>
+      We have thousands of videos created by expert teachers on web design and
+      front end development. Our library is continually refreshed with the
+      latest on web technology so you will never fall behind.
+    </p>
+    <hr />
+  </div>
+);
 
 const About = () => (
-<div className="main-content">
-<h2>About</h2>
-<p>
-  The front end course directory lists many of the courses we teach on
-  HTML, CSS, JavaScript and more! Be sure to visit the Teachers section
-  to view a list of our talented teachers. Or visit the Courses section
-  and select a topic -- HTML, CSS, or JavaScript -- to see a list of our
-  courses.
-</p>
-</div>
-)
- 
-const Teachers = (props) => {
-    let teachers = TeacherList.map((teacher, index) => {
-      return (
-        <li key={index} className="teacher">
-          <img className="teacher-img " src={teacher.img_src} alt="Teacher" />
-          <h3>{teacher.name}</h3>
-          <p>{teacher.bio} </p>
-        </li>
-      );
-     });
+  <div className="main-content">
+    <h2>About</h2>
+    <p>
+      The front end course directory lists many of the courses we teach on HTML,
+      CSS, JavaScript and more! Be sure to visit the Teachers section to view a
+      list of our talented teachers. Or visit the Courses section and select a
+      topic -- HTML, CSS, or JavaScript -- to see a list of our courses.
+    </p>
+  </div>
+);
 
-   return (
+const Teachers = props => {
+  let teachers = TeacherList.map((teacher, index) => {
+    return (
+      <li key={index} className="teacher">
+        <img className="teacher-img " src={teacher.img_src} alt="Teacher" />
+        <h3>{teacher.name}</h3>
+        <p>{teacher.bio} </p>
+      </li>
+    );
+  });
 
+  return (
     <div className="main-content">
-    <h2>Teachers</h2>
-    <ul className="group">{teachers}</ul>
-  </div>   );
-}
-const HTML =  () => {
+      <h2>Teachers</h2>
+      <ul className="group">{teachers}</ul>
+    </div>
+  );
+};
+const HTML = () => {
   let CurrentList = null; //es la lista actual
-  return(  
-  HTMLCourses.map(item => {
+  return HTMLCourses.map(item => {
     return (
       <li key={item.id} className="course media group">
         <img className="course-img" src={item.img_src} alt="course" />
         <div>
-          <h3>
-            {item.title}
-          </h3>
-          <p>
-            {item.description}
-          </p>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
         </div>
       </li>
     );
-})
-   ) 
-}
-const CSS =  () => {
+  });
+};
+const CSS = () => {
   let CurrentList = null; //es la lista actual
-  return(  
-  CSSCourses.map(item => {
+  return CSSCourses.map(item => {
     return (
       <li key={item.id} className="course media group">
         <img className="course-img" src={item.img_src} alt="course" />
         <div>
-          <h3>
-            {item.title}
-          </h3>
-          <p>
-            {item.description}
-          </p>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
         </div>
       </li>
     );
-})
-   ) 
-}
-const JavaScript =  () => {
+  });
+};
+const JavaScript = () => {
   let CurrentList = null; //es la lista actual
-  return(  
-  JSCourses.map(item => {
+  return JSCourses.map(item => {
     return (
       <li key={item.id} className="course media group">
         <img className="course-img" src={item.img_src} alt="course" />
         <div>
-          <h3>
-            {item.title}
-          </h3>
-          <p>
-            {item.description}
-          </p>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
         </div>
       </li>
     );
-})
-   ) 
-  }
+  });
+};
 
 const Courses = () => {
-   return (
-      <div className="main-content courses">
-         <div className="course-header group">
-         <h2>Courses</h2><br/><br/>
-          <ul className="course-nav"> 
-             <li>  <NavLink exact to="/courses/html"> HTML </NavLink> </li>
-             <li>  <NavLink to="/courses/css"> CSS </NavLink> </li>
-             <li>  <NavLink to="/courses/javascript"> JavaScript </NavLink> </li>            
-          </ul><br/><br/>
+  return (
+    <div className="main-content courses">
+      <div className="course-header group">
+        <h2>Courses</h2>
+        <br />
+        <br />
+        <ul className="course-nav">
+          <li>
+            {" "}
+            <NavLink exact to="/courses/html">
+              {" "}
+              HTML{" "}
+            </NavLink>{" "}
+          </li>
+          <li>
+            {" "}
+            <NavLink to="/courses/css"> CSS </NavLink>{" "}
+          </li>
+          <li>
+            {" "}
+            <NavLink to="/courses/javascript"> JavaScript </NavLink>{" "}
+          </li>
+        </ul>
+        <br />
+        <br />
 
-          <Switch> 
-            <Route exact path= "/courses" 
-                   render={()=><Redirect to="/courses/html" /> } />
+        <Switch>
+          <Route
+            exact
+            path="/courses"
+            render={() => <Redirect to="/courses/html" />}
+          />
 
-            <Route path = "/courses/html" component = {HTML}/>
-            <Route path = "/courses/css" component = {CSS}/>
-            <Route path = "/courses/javascript" component = {JavaScript}/>
-          </Switch>
-          </div>
+          <Route path="/courses/html" component={HTML} />
+          <Route path="/courses/css" component={CSS} />
+          <Route path="/courses/javascript" component={JavaScript} />
+        </Switch>
       </div>
-   );   
-}
+    </div>
+  );
+};
 
-const NotFoundPage = (props) => {
-   return (
-      <div> <h2> Error 404! </h2> </div>
-   );
-}
+const NotFoundPage = props => {
+  return (
+    <div>
+      {" "}
+      <h2> Error 404! </h2>{" "}
+    </div>
+  );
+};
 
 const App = () => (
   <BrowserRouter>
-  <div className="main-content">
-    <div data-reactroot className="container">
-      <header>
+    <div className="main-content">
+      <div data-reactroot className="container">
+        <header>
           <span className="icn-logo">
             <i className="material-icons">code</i>
           </span>
           <ul className="main-nav">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li> 
-        <li><NavLink to="/teachers">Teachers</NavLink></li> 
-        <li><NavLink to="/courses">Courses</NavLink></li> 
-                
-      </ul>
-      </header>
-      <hr/>
-      <Switch>
-         <Route exact path="/" component={Home}/>
-         <Route path="/about" component={About}/> 
-         <Route path="/teachers" component={Teachers}/> 
-         <Route path="/courses" component={Courses}/> 
-         
-         <Route component={NotFoundPage}/>
-      </Switch>
-    </div>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/teachers">Teachers</NavLink>
+            </li>
+            <li>
+              <NavLink to="/courses">Courses</NavLink>
+            </li>
+          </ul>
+        </header>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/teachers" component={Teachers} />
+          <Route path="/courses" component={Courses} />
+
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
     </div>
   </BrowserRouter>
-)
-
+);
 
 export default App;
