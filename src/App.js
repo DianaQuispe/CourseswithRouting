17583c5dc1b1cb20b6211493320bd4f724d018e7
DianaQuispe <danaalienmas@gmail.com>
2017-10-09 12:25:39 -0500

    about, home y courses hechos

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css'; 
 
-///
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+  NavNavLink,
+  Redirect
+} from 'react-router-dom'
 "use stritc";
+
 
 const TeacherList = [
   {
@@ -210,52 +218,125 @@ const JSCourses = [
 
 
 
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  NavLink,
-  NavNavLink,
-  Redirect
-} from 'react-router-dom'
 
 const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
+  <div className="main-content home">
+        <h2>Front End Course Directory</h2>
+
+        <p>
+          This fun directory is a project for the <em>React Router Basics</em>{" "}
+          course on Treehouse.
+        </p>
+        <p>
+          Learn front end web development and much more! This simple directory
+          app offers a preview of our course library. Choose from many hours of
+          content, from HTML to CSS to JavaScript. Learn to code and get the
+          skills you need to launch a new career in front end web development.
+        </p>
+        <p>
+          We have thousands of videos created by expert teachers on web design
+          and front end development. Our library is continually refreshed with
+          the latest on web technology so you will never fall behind.
+        </p>
+        <hr />
+      </div>
 )
 
 const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
+<div className="main-content">
+<h2>About</h2>
+<p>
+  The front end course directory lists many of the courses we teach on
+  HTML, CSS, JavaScript and more! Be sure to visit the Teachers section
+  to view a list of our talented teachers. Or visit the Courses section
+  and select a topic -- HTML, CSS, or JavaScript -- to see a list of our
+  courses.
+</p>
+</div>
 )
  
 const Teachers = (props) => {
+    let teachers = TeacherList.map((teacher, index) => {
+      return (
+        <li key={index} className="teacher">
+          <img className="teacher-img " src={teacher.img_src} alt="Teacher" />
+          <h3>{teacher.name}</h3>
+          <p>{teacher.bio} </p>
+        </li>
+      );
+     });
+
    return (
-      <div> <h2> Teachers </h2> </div>
-   );
+
+    <div className="main-content">
+    <h2>Teachers</h2>
+    <ul className="group">{teachers}</ul>
+  </div>   );
 }
 const HTML =  () => {
-   return (
-      <div> <h2> HTML </h2> </div>
-   );
+  let CurrentList = null; //es la lista actual
+  return(  
+  HTMLCourses.map(item => {
+    return (
+      <li key={item.id} className="course media group">
+        <img className="course-img" src={item.img_src} alt="course" />
+        <div>
+          <h3>
+            {item.title}
+          </h3>
+          <p>
+            {item.description}
+          </p>
+        </div>
+      </li>
+    );
+})
+   ) 
 }
 const CSS =  () => {
-   return (
-      <div> <h2> CSS </h2> </div>
-   );
+  let CurrentList = null; //es la lista actual
+  return(  
+  CSSCourses.map(item => {
+    return (
+      <li key={item.id} className="course media group">
+        <img className="course-img" src={item.img_src} alt="course" />
+        <div>
+          <h3>
+            {item.title}
+          </h3>
+          <p>
+            {item.description}
+          </p>
+        </div>
+      </li>
+    );
+})
+   ) 
 }
 const JavaScript =  () => {
-   return (
-      <div> <h2> JavaScript </h2> </div>
-   );
-}
+  let CurrentList = null; //es la lista actual
+  return(  
+  JSCourses.map(item => {
+    return (
+      <li key={item.id} className="course media group">
+        <img className="course-img" src={item.img_src} alt="course" />
+        <div>
+          <h3>
+            {item.title}
+          </h3>
+          <p>
+            {item.description}
+          </p>
+        </div>
+      </li>
+    );
+})
+   ) 
+  }
 
 const Courses = () => {
    return (
       <div>
-          <h2> Courses </h2> 
           <ul> 
              <li>  <NavLink exact to="/courses/html"> HTML </NavLink> </li>
              <li>  <NavLink to="/courses/css"> CSS </NavLink> </li>
